fbe46e24fc5a52b6bf0b709bffba5242ce85881b
ZARAKANG <dbkang@wildthing.co.kr>
2021-11-30 17:02:50 +0900

    personal
    test

---
+++ src/com/dbkang/TernaryOperatorTest.java
@@ -0,0 +1,12 @@
+package com.dbkang;
+
+public class TernaryOperatorTest {
+	public static void main(String[] args) {
+		int member;
+		member = 10;
+		
+		char isAdult = (member > 20) ? 'T' : 'F';
+		System.out.println("gilDong isAdult Value is [" + isAdult + "]");
+	}
+
+}
